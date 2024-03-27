395a3e82a18730d4fd2ec5f6d4f4586cde114874
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-03-27 20:10:48 +0100

    added form with form class and template rendering as well as project list and set up new project functionality

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,101 @@
-// Code goes here!
+abstract class ProjectComponent {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+    constructor(templateId: string, hostElementId: string) {
+        this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
+        this.hostElement = document.getElementById(hostElementId)! as HTMLDivElement;
+         const importedNode = document.importNode(
+           this.templateElement.content,
+           true
+         );
+         this.element = importedNode.firstElementChild as HTMLElement;
+         this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+    
+    }
+
+
+    
+
+    class Project {
+      constructor(
+        public title: string,
+        public description: string,
+        public people: number,
+        public templateElement: HTMLTemplateElement,
+      ) {}
+    }
+
+   
+
+ 
+
+
+class ProjectInput extends ProjectComponent{
+    form: HTMLFormElement;
+    constructor(){
+        super('project-input','app');
+        this.form = this.element as HTMLFormElement;
+        this.form.addEventListener('submit',this.submitHandler.bind(this));
+    }
+   
+        
+        
+    
+    private submitHandler(event: Event){
+        event.preventDefault();
+        const titleInput = document.getElementById('title') as HTMLInputElement;
+        const descriptionInput = document.getElementById('description') as HTMLInputElement;
+        const peopleInput = document.getElementById('people') as HTMLInputElement;
+
+        const title = titleInput.value;
+        const description = descriptionInput.value;
+        const people = peopleInput.value;
+        const newProject = new Project(title,description,+people,document.getElementById('single-project') as HTMLTemplateElement);
+        projectList.assignedProjects.push(newProject);
+        projectList.renderList();
+
+    }
+   
+    
+}
+
+ class ProjectList extends ProjectComponent {
+   assignedProjects: Project[];
+   ulElement: HTMLUListElement;
+   constructor() {
+     super("project-list", "app");
+     this.assignedProjects = [];
+     
+     this.ulElement = this.element.querySelector("ul") as HTMLUListElement;
+     this.log();
+   }
+   log() {
+     console.log(this);
+   }
+   renderList() {
+     const projectElement = document.importNode(
+       this.assignedProjects[this.assignedProjects.length - 1].templateElement
+         .content,
+       true
+     );
+     const listLElement = projectElement.firstElementChild as HTMLLIElement;
+        listLElement.textContent = this.assignedProjects[
+        this.assignedProjects.length - 1
+        ].title;
+
+     this.ulElement.appendChild(listLElement);
+   }
+ }
+
+
+
+
+
+
+
+const projectList = new ProjectList();
+const projectInput = new ProjectInput();
+   
+
